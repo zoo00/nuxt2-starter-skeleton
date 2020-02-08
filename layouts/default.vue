a05f4e466e8dd0c7f006c1c99933d4034e35cda5
shingo <shingo.mizuno@gmail.com>
2020-02-09 01:19:03 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    nuxt
+</template>
+
+<script>
+  export default {
+    name: 'default',
+    components: {}
+  }
+</script>
+
+<style lang="stylus" scoped>
+</style>
